2307625fb14eebf383360c72fb4c80debe134575
obidjon <Marufjonoff>
2022-07-16 12:05:02 +0500

    new commit

--- test/calculate_area_test.dart
+++ test/calculate_area_test.dart
@@ -1,12 +1,10 @@
+import 'package:calculate_area/pages/calculate_rectangle_area.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:calculate_area/calculate_area.dart';
 
 void main() {
   test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+
   });
 }
